9070fd74ee68526ff33542a0e47df8cfaf43dda4
pansu <34263218+pansusu@users.noreply.github.com>
2023-02-13 12:09:43 +0800

    调整页面布局

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ document.head.appendChild(meta)
 async function setupApp() {
     const app = createApp(App);
     app.use(SocketIO, {
-        connection: 'http://localhost',
+        connection: 'http://101.42.39.244',
     });
 
     await setupRouter(app);
